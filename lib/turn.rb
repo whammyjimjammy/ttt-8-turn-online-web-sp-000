d42a08e14e619686637c9687547dc95d461d2bb2
Jake ORiley <ti1795sv@gmail.com>
2019-06-01 18:45:57 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -1,14 +1,25 @@
 
 
-def display_board
-  puts "   |   |   "
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
   puts "-----------"
-  puts "   |   |   "
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
 
 
 def turn(board)
   puts "Please enter 1-9:"
 end
+
+def valid_move?(board,index)
+  index.between?(0,8) && position_taken?(board,index)
+end
+
+def position_taken?(board,index)
+  board[index] == " " || board[index] == "" || board[index] == nil
+end
+
+def move(board,index)
